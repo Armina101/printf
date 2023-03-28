46557923c9f21d81620cd4f31922e2bce9a278be
Afolabi Aminat <Afolabijolayemi@gmail.com>
2023-03-28 11:25:03 -0700

    Final update2

--- _printf.c
+++ _printf.c
@@ -32,10 +32,10 @@ int _printf(const char *format, ...)
 		else
 		{
 			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
+			flags = flags_calc(format, &i);
+			width = width_calc(format, &i, list);
+			precision = precision_calc(format, &i, list);
+			size = size_calc(format, &i);
 			++i;
 			printed = handle_print(format, &i, list, buffer,
 				flags, width, precision, size);
